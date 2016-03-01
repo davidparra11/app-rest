079fc4c901162a97b08a7dfed79e27fd79ae0f43
davidparra11 <david.parra@utp.edu.co>
2016-03-01 17:50:41 -0500

    fixing isAuth.js file

--- api/policies/isAuth.js
+++ api/policies/isAuth.js
@@ -1,13 +1,16 @@
 module.exports = function isAuth (req, res, next) {
 
-var APPROVED_API_KEY = process.env.APPROVED_API_KEY || '';
+	var APPROVED_API_KEY = process.env.APPROVED_API_KEY || '';
 
-  if(process.env.APPROVED_API_KEY == req.param('token'))
+	if(process.env.APPROVED_API_KEY == req.param('token'))
     next();
-  else{
-  	console.warn({"code":401,"message":"Invalid autentication"});
-  	res.send({"code":401, "message":"Invalid autentication"});
+	else{
+  	console.warn(401, {"message":"Invalid autentication"});
+  	res.send(401, {"message":"Invalid autentication"});
 	}
 
-  };
+};
+
+
+
 
