82985ecabb5a808b8f99aab98c294fc941da68e8
hash <hash.key66@gmail.com>
2016-03-15 22:59:24 -0500

    cambios en models

--- config/models.js
+++ config/models.js
@@ -28,7 +28,7 @@ module.exports.models = {
   *                                                                          *
   ***************************************************************************/
   migrate    : 'safe',             //safe - alter  - drop  
-    //connection : 'mongoProduction',
+  connection : 'mongoProduction',
     //connection : 'productionPosgrest',
     //schema     : false,             // habilita el uso de esquemas en DB
 
