2a651fa8c6fc0aaee6bac200e3676a5006ceb748
yzapata <hash.key66@gmail.com>
2016-04-15 09:10:03 -0500

    reformat code

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,27 @@
-var gulp=require('gulp');
-var spawn=require('child_process').spawn,node;
+var gulp = require('gulp');
+var spawn = require('child_process').spawn, node;
 
 
-
-gulp.task('server', function() {
-  if (node) node.kill()
-  node = spawn('node', ['app.js'], {stdio: 'inherit'})
-  node.on('close', function (code) {
-    if (code === 8) {
-      gulp.log('Error detected, waiting for changes...');
-    }
-  });
+gulp.task('server', function () {
+    if (node) node.kill()
+    node = spawn('node', ['app.js'], {stdio: 'inherit'})
+    node.on('close', function (code) {
+        if (code === 8) {
+            gulp.log('Error detected, waiting for changes...');
+        }
+    });
 });
 
-gulp.task('default', function() {
-  gulp.run('server')
-  gulp.watch(['api/controllers/*.js','*.html','*.js'], function() {
+gulp.task('default', function () {
     gulp.run('server')
-  });
+    gulp.watch(['api/controllers/*.js', '*.html', '*.js'], function () {
+        gulp.run('server')
+    });
 
 });
 
 var eslint = require('gulp-eslint');
-var files = ['api/**/*.js','!node_modules/**', '!api/responses/*.js'];
+var files = ['api/**/*.js', '!node_modules/**', '!api/responses/*.js'];
 
 gulp.task('lint', function () {
     return gulp.src(files)
