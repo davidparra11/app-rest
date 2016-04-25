43efa6e3e59c664dd09c9a68bee897938e43d2d6
David Parra Ospina <david.parra@utp.edu.co>
2016-04-25 17:59:09 -0500

    Topic collections has benn changed by Friend´s name

---
+++ api/models/Friends.js
@@ -0,0 +1,35 @@
+'use strict';
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+    tableName: 'friends',
+
+
+    attributes: {
+
+        followerId: {
+            type: 'string',
+            required: true,
+            unique: true
+        },
+
+        friendsId: {
+            type: 'string',
+            required: true,
+            unique: true
+        },
+
+        relations: {
+      		model: 'user',
+      		via: 'friends'
+   		 }
+
+        
+    }
+};
