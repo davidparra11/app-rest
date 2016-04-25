83c07b076f829158058cb10ba0a8e886d5dafcf2
David Parra Ospina <david.parra@utp.edu.co>
2016-04-25 15:15:24 -0500

    deletting Topic model

--- api/models/User.js
+++ api/models/User.js
@@ -82,10 +82,6 @@ module.exports = {
             defaultsTo: 'http://vignette3.wikia.nocookie.net/the-enigma-corporation/images/0/01/Users-User-icon.png/revision/latest?cb=20140213102228'
         },
 
-        topics: {
-            collection: 'topics',
-            via: 'owner'
-        }
         // The timestamp when the the user last logged in
         // (i.e. sent a username and password to the server)
         lastLoggedIn: {
