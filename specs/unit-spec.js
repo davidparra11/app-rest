3e0c5c5f94a0878020d60d9d2c742c0ce2fbd1a2
David Parra Ospina <david.parra@utp.edu.co>
2016-05-05 12:11:51 -0500

    specs changes for a bride var

--- specs/unit-spec.js
+++ specs/unit-spec.js
@@ -1,4 +1,4 @@
-var calculator = require("../api/utilities/Utils");
+var utilidades = require("../api/utilities/Utils");
 
 describe("Utils func Test", function () {
   it("retrn a object", function () {
@@ -6,7 +6,7 @@ describe("Utils func Test", function () {
         interCode: '+57',
         phoneNumber: '3213214455'
       };
-    var product = calculator.phoneSplit('+573213214455');
+    var product = utilidades.phoneSplit('+573213214455');
     expect(product).toEqual(foo);
     expect(product).not.toBeNull();
     //expect(product).not.toThrow();
@@ -15,9 +15,9 @@ describe("Utils func Test", function () {
   it("retrn a array of numbers", function () {
   	var array = ['573155397722', '673123456789'];
 
-    var arrayreturned = calculator.convertString(array);
+    var arrayreturned = utilidades.convertString(array);
     console.log(arrayreturned);
-   // expect(function () { calculator.convertString(array); }).toThrowError(Error);
+   // expect(function () { utilidades.convertString(array); }).toThrowError(Error);
     //expect(arrayreturned).not.toBeNull();.toBe(b);
     expect(arrayreturned).not.toBe(null);
     //expect(arrayreturned).toContain('3155397722');
