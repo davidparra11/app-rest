e0615a685e4afe1a5f0bd51563571b68ad217f58
davidparra <david.parra@utp.edu.co>
2016-06-01 22:13:41 -0500

    adding before Each func to FriednController.test

--- test/integration/controllers/FriendController.test.js
+++ test/integration/controllers/FriendController.test.js
@@ -4,43 +4,50 @@ var camila = new ObjectId("57280ea90e4807bc03e33cab");
 
 describe('#Follow() method', function() {
 
-    it('it returns an WARN, when pass incorrect params', function(done) {
-      var phoneNumberString = "1234567899";
-      var toFollowString = "3155397722";
-      request(sails.hooks.http.app)
-        .put('/follows/')
-        .send({
-          phoneNumber: phoneNumberString,
-          toFollow: toFollowString
-        })
-        .expect(400, done);
-    });
+  var phoneNumberString;
+  var toFollowString;
 
-    it('it returns an WARN, when left params', function(done) {
-      var phoneNumberString = "1234567899";
-      var toFollowString = "3155397722";
-      request(sails.hooks.http.app)
-        .put('/follows/')
-        .send({
-          phoneNumber: phoneNumberString
-        })
-        .expect(400, done);
-    });
-
-    it('it show warn because the one of them dont exist', function(done) {
-       var phoneNumberString = "1234567890";
-      var toFollowString = "3155397729";
-     
-      request(sails.hooks.http.app)
-        .put('/follows/')
-        .send({
-          phoneNumber: phoneNumberString,
-          toFollow: toFollowString
-        })
-        .expect(400)
-        .end(function(err, res) {
-          if (err) return done(err);
-          done();
-        });
-    });
+  beforeEach(function() {
+    phoneNumberString = "1234567899";
+    toFollowString = "3155397722";
   });
+
+
+  it('it returns an WARN, when pass incorrect params', function(done) {
+    
+    request(sails.hooks.http.app)
+      .put('/follows/')
+      .send({
+        phoneNumber: phoneNumberString,
+        toFollow: toFollowString
+      })
+      .expect(400, done);
+  });
+
+  it('it returns an WARN, when left params', function(done) {
+    
+    request(sails.hooks.http.app)
+      .put('/follows/')
+      .send({
+        phoneNumber: phoneNumberString
+      })
+      .expect(400, done);
+  });
+
+  it('it show warn because the one of them dont exist', function(done) {
+    var phoneNumberString = "1234567890";
+    var toFollowString = "3155397729";
+
+    request(sails.hooks.http.app)
+      .put('/follows/')
+      .send({
+        phoneNumber: phoneNumberString,
+        toFollow: toFollowString
+      })
+      .expect(400)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
